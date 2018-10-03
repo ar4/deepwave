9c7d35cd1ed2efb363ecc6fc3496f37780e69f20
Alan Richardson <arichar@tcd.ie>
2018-10-03 13:22:26 +0100

    Include CUDA files in MANIFEST.in
    When installing from PyPI rather than the Github development version,
    the CUDA files were previously not included and so building on the GPU
    would fail. These files are now explicitly added to the MANIFEST to
    ensure that they are included in the source distribution.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ else:
 
 setuptools.setup(
     name="deepwave",
-    version="0.0.3",
+    version="0.0.4",
     author="Alan Richardson",
     author_email="alan@ausargeo.com",
     description="Wave propagation modules for PyTorch.",
