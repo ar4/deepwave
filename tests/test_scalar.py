Fix bug in scalar_born's gradient at source
Scalar_born model gradients at source locations were incorrect
when the timestep interval was greater than the CFL limit. This
was caused by accumulating the gradient there at every timestep
when it should only have been accumulated at multiples of the
step ratio.

tests/test_scalar.py
@@ -234,6 +234,11 @@ def test_gradcheck_2d_8th_order():
                      prop_kwargs={'accuracy': 8})
 
 
+def test_gradcheck_2d_cfl():
+    """Test gradcheck with a timestep greater than the CFL limit."""
+    run_gradcheck_2d(propagator=scalarprop, dt=0.002, atol=3e-7)
+
+
 def test_gradcheck_2d_one_shot():
     """Test gradcheck with one shot."""
     run_gradcheck_2d(propagator=scalarprop, num_shots=1)
@@ -563,7 +568,8 @@ def run_gradcheck(c, dc, freq, dx, dt, nx,
                   psix_m1_requires_grad=True,
                   psiy_m1_requires_grad=True,
                   zetax_m1_requires_grad=True,
-                  zetay_m1_requires_grad=True):
+                  zetay_m1_requires_grad=True,
+                  atol=1e-8, rtol=1e-5):
     """Run PyTorch's gradcheck to test the gradient."""
     torch.manual_seed(1)
     if device is None:
@@ -625,7 +631,7 @@ def run_gradcheck(c, dc, freq, dx, dt, nx,
                                           psix_m1, psiy_m1, zetax_m1,
                                           zetay_m1, nt, 1, True),
                              nondet_tol=1e-3, check_grad_dtypes=True,
-                             atol=1e-8, rtol=1e-5)
+                             atol=atol, rtol=rtol)
 
 
 def run_gradcheck_2d(c=1500, dc=100, freq=25, dx=(5, 5), dt=0.001,

tests/test_scalar_born.py
@@ -314,6 +314,12 @@ def test_born_gradcheck_2d_8th_order():
                           prop_kwargs={'accuracy': 8})
 
 
+def test_born_gradcheck_2d_cfl():
+    """Test gradcheck with a timestep greater than the CFL limit."""
+    run_born_gradcheck_2d(propagator=scalarbornprop, dt=0.002, atol=2e-7,
+                          rtol=1e-8)
+
+
 def test_born_gradcheck_2d_one_shot():
     """Test gradcheck with one shot."""
     run_born_gradcheck_2d(propagator=scalarbornprop, num_shots=1)
@@ -529,7 +535,7 @@ def run_born_gradcheck(c, dc, freq, dx, dt, nx,
                        psiysc_m1_requires_grad=True,
                        zetaxsc_m1_requires_grad=True,
                        zetaysc_m1_requires_grad=True,
-                       ):
+                       atol=1e-8, rtol=1e-5):
     """Run PyTorch's gradcheck."""
     torch.manual_seed(1)
     if device is None:
@@ -619,8 +625,7 @@ def run_born_gradcheck(c, dc, freq, dx, dt, nx,
                                           True
                                           ),
                              nondet_tol=1e-3, check_grad_dtypes=True,
-                             atol=1e-8, rtol=1e-5
-                             )
+                             atol=atol, rtol=rtol)
 
 
 def run_born_gradcheck_2d(c=1500, dc=100, freq=25, dx=(5, 5), dt=0.001,