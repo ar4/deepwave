30a6a72e31928a52198fdba213cc7d54225708a6
Alan Richardson <arichar@tcd.ie>
2022-06-18 20:59:12 +0100

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Alan Richardson'
 author = 'Alan Richardson'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.11'
+release = 'v0.0.12'
 
 
 # -- General configuration ---------------------------------------------------
