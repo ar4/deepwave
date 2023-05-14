846df7bb9bb3a5ebc9bcbeedb1d0b4abf25d5279
Alan Richardson <arichar@tcd.ie>
2023-05-14 20:05:10 +0100

    change Python version in GitHub action to 3.10 as torch.compile does not support 3.11

--- default.nix
+++ default.nix
@@ -10,40 +10,14 @@
 
 with import <nixpkgs> { };
 
-let
-  linePatch = ''
-    import os
-    os.environ['PATH'] = os.environ['PATH'] + ':${ninja}/bin'
-  '';
-in
 python3Packages.buildPythonPackage rec {
   pname = "deepwave";
-  version = "0.0.18";
+  version = "0.0.19.dev1";
   format = "pyproject";
 
   src = ./.;
 
-  # unable to find ninja although it is available, most likely because it looks for its pip version
-  postPatch = ''
-    substituteInPlace setup.cfg --replace "ninja" ""
-
-    # Adding ninja to the path forcibly
-    mv src/deepwave/__init__.py tmp
-    echo "${linePatch}" > src/deepwave/__init__.py
-    cat tmp >> src/deepwave/__init__.py
-    rm tmp
-  '';
-
-  # The source files are compiled at runtime and cached at the
-  # $HOME/.cache folder, so for the check phase it is needed to
-  # have a temporary home. This is also the reason ninja is not
-  # needed at the nativeBuildInputs, since it will only be used
-  # at runtime.
-  preBuild = ''
-    export HOME=$(mktemp -d)
-  '';
-
-  propagatedBuildInputs = with python3Packages; [ torch pybind11 ];
+  propagatedBuildInputs = with python3Packages; [ torch ];
 
   nativeCheckInputs = [
     which
